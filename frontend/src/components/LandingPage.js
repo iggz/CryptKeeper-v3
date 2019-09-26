33cd3235eea5db92cf50a35942cba6d42b9fd8c1
igor popenov <ipopenov@protonmail.com>
2019-09-26 19:15:17 -0400

    Centered image on landing page

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -66,7 +66,7 @@ class LandingPage extends Component {
                                 <></>
                             ) }
                     </Box>
-                    <div style={ { height: 600, background: '#09171E' } }>
+                    <div style={ { height: 600, background: '#09171E', textAlign: 'center' } }>
                         <img src={ Logo } alt={ 'CryptKeeper' }></img>
 
                     </div>
